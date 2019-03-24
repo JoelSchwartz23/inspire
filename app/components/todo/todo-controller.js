0ccba9258d86633c8654335ccfc9a7805fdfe00a
Joel Schwartz <nederlands18@gmail.com>
2019-03-23 19:20:37 -0600

    push

--- app/components/todo/todo-controller.js
+++ app/components/todo/todo-controller.js
@@ -2,20 +2,14 @@ import TodoService from "./todo-service.js";
 
 var todoService = new TodoService
 
-// Use this getTodos function as your callback for all other edits
 function getTodos() {
-	//FYI DONT EDIT ME :)
 	todoService.getTodos(drawTodo)
 }
 
 
 function drawTodo(todos) {
-	//WHAT IS MY PURPOSE?
-	//BUILD YOUR TODO TEMPLATE HERE
 	console.log(todos)
 	var template = `<b> To do list : </b>  <ins>${todos.length}</ins>`
-
-	//DONT FORGET TO LOOP
 	todos.forEach(todo => {
 		template += `
 			<li>
@@ -29,38 +23,23 @@ function drawTodo(todos) {
 
 export default class TodoController {
 	constructor() {
-		// IF YOU WANT YOUR TODO LIST TO DRAW WHEN THE PAGE FIRST LOADS WHAT SHOULD YOU CALL HERE???
 		todoService.getTodos(drawTodo)
 	}
 
 	addTodoFromForm(e) {
-		e.preventDefault() // <-- hey this time its a freebie don't forget this
-		// TAKE THE INFORMATION FORM THE FORM
+		e.preventDefault()
 		var form = e.target;
 		var todo = {
 			description: form.description.value
-			// DONT FORGET TO BUILD YOUR TODO OBJECT
 		}
-		//PASSES THE NEW TODO TO YOUR SERVICE
-		//DON'T FORGET TO REDRAW THE SCREEN WITH THE NEW TODO
-		//YOU SHOULDN'T NEED TO CHANGE THIS
 		todoService.addTodo(todo, getTodos)
-		//^^^^^^^ EXAMPLE OF HOW TO GET YOUR TOODOS AFTER AN EDIT
 	}
 
 	toggleTodoStatus(todoId) {
-		// let todo = {
-		// 	_id: todoid,
-		// 	completed: document.getElementById('todoid')
-		// }
 		todoService.toggleTodoStatus(todoId, getTodos)
-		// asks the service to edit the todo statuodos)
-		// YEP THATS IT FOR ME
 	}
 
 	deleteTodo(todoId) {
-		// ask the service to run the remove todo with this id
 		todoService.deleteTodo(todoId, drawTodo)
-		// ^^^^ THIS LINE OF CODE PROBABLY LOOKS VERY SIMILAR TO THE toggleTodoStatus
 	}
 }
